a98635581c5d247bfa991504caf3a9d218114e89
PatrycjaWiza <patrycja.wiza@gmail.com>
2022-05-25 20:43:51 +0200

    task 1
    throttle 1 sek (tracking currentTime)

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,19 +1,30 @@
+// loadash throttle
+var _ = require('lodash');
+var loadashthrottle = require('lodash.throttle');
+
+// constants
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
-// track currentTime
-player.on('timeupdate', function () {
-  this.getCurrentTime()
-    .then(function (seconds) {
-      localStorage.setItem('videoplayer-current-time', seconds);
-    })
-    .catch(function (error) {});
-  console.log(localStorage);
-});
+// track currentTime with throttle = 1 sec
+
+player.on(
+  'timeupdate',
+  _.throttle(() => {
+    player
+      .getCurrentTime()
+      .then(function (seconds) {
+        localStorage.setItem('videoplayer-current-time', seconds);
+      })
+      .catch(function (error) {});
+    console.log(localStorage);
+  }, 1000),
+);
 
 // setting currentTime
 player.on('play', function () {
   const currentTime = localStorage.getItem('videoplayer-current-time');
+
   this.setCurrentTime(currentTime)
     .then(function (seconds) {})
     .catch(function (error) {
