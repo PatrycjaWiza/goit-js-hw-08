09ae47d01fff21141713ba881091b82a31597eed
PatrycjaWiza <patrycja.wiza@gmail.com>
2022-05-25 19:37:52 +0200

    task 1
    tracking current time
    setting it to local storage
    setting player time from the last tracked in local storage

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,27 @@
-// czas odtwarzania do local storage &&
-// after page refreshes video is continued
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+// track currentTime
+player.on('timeupdate', function () {
+  this.getCurrentTime()
+    .then(function (seconds) {
+      localStorage.setItem('videoplayer-current-time', seconds);
+    })
+    .catch(function (error) {});
+  console.log(localStorage);
+});
+
+// setting currentTime
+player.on('play', function () {
+  const currentTime = localStorage.getItem('videoplayer-current-time');
+  this.setCurrentTime(currentTime)
+    .then(function (seconds) {})
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          break;
+        default:
+          break;
+      }
+    });
+});
