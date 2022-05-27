9c2b98788a61fce90fa2d44d8e0d8b975db77b0e
PatrycjaWiza <patrycja.wiza@gmail.com>
2022-05-27 22:22:28 +0200

    Update 03-feedback.js
    `.localStorage updated with form values
    2.if localStorage !==null form fields are updated
    3.submit cleares localStorage & removes field values & sends form values to console
    4. throttle does not working - must be set to 500ms

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,54 @@
+// lodash library
+const _ = require('lodash');
+// constants
+const form = document.querySelector('form');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+//storage functions
+const save = (key, value) => {
+  try {
+    const stringifyJSON = JSON.stringify(value);
+    localStorage.setItem(key, stringifyJSON);
+  } catch (error) {
+    console.error('Error message: ', error.message);
+  }
+};
+const load = key => {
+  try {
+    const getStorageItem = localStorage.getItem(key);
+    return getStorageItem === null ? undefined : JSON.parse(getStorageItem);
+  } catch (error) {
+    console.error('Error message: ', error.message);
+  }
+};
+//get data from form on input
+form.addEventListener(
+  'input',
+  _.throttle(() => {
+    saveInput();
+  }, 500),
+);
+//clear form & localStorage on submition
+form.addEventListener('submit', clear);
 
+// updates form values from localStorage
+try {
+  form.email.value = load(LOCALSTORAGE_KEY).email;
+  form.message.value = load(LOCALSTORAGE_KEY).message;
+} catch (error) {
+  console.log('Local storage is empty:', error.message);
+}
+// setting to localStorage
+function saveInput(e) {
+  const { email, message } = e.currentTarget;
+  const object = { email: email.value, message: message.value };
+  save(LOCALSTORAGE_KEY, object);
+}
+
+// clear localStorage & form and load data to console
+function clear(e) {
+  e.preventDefault();
+  const { email, message } = e.currentTarget;
+  console.log(`email: ${email.value}, message: ${message.value}`);
+  localStorage.clear();
+  e.currentTarget.reset();
+}
